c5fd9f1e74bcc9b2c7ecd5e35b802e92b71c735a
jakaria98 <jhossain439@gmail.com>
2021-03-30 00:48:36 +0600

    transaction router created

---
+++ routers/transactionRoute.js
@@ -0,0 +1,9 @@
+const router = require("express").Router;
+
+router.get("/", (req, res) => {});
+router.post("/", (req, res) => {});
+router.get("/:transactionId", (req, res) => {});
+router.put("/:transactionId", (req, res) => {});
+router.delete("/:transactionId", (req, res) => {});
+
+module.exports = router;
