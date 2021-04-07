65c5d157c314659e4a6542ea8447501a7b858b50
jakaria98 <jhossain439@gmail.com>
2021-04-08 00:01:48 +0600

    navbar added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,11 +6,13 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
+import Navigation from "./navigation/Navigation";
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="container">
+          <Navigation />
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/register" component={Register} />
